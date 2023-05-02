865b2d0c48564f8ea82e52627b1a386e4f65ab12
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-05-02 17:53:01 +0900

    upto springbean search for duplicate

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,46 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+	@Test
+	@DisplayName("빈 이름으로 조회")
+	void findBeanByName() {
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("이름없이 타입으로 조회")
+	void findBeanByType() {
+		MemberService memberService = ac.getBean(MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("구체 타입으로 조회")  // 역할에 의존해야 하기 때문에 안좋다.
+	void findBeanByName2() {
+		MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+
+	@Test
+	@DisplayName("빈 이름으로 조회X")
+	void findBeanByNameX() {
+//		MemberService xxx = ac.getBean("xxx", MemberService.class);
+		assertThrows(NoSuchBeanDefinitionException.class,
+				() -> ac.getBean("xxx", MemberService.class));  // 오른쪽 실행시 왼쪽이 떠야한다는 뜻.
+	}
+
+}
