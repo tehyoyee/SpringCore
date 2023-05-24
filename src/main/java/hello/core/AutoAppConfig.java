37a2e88c7f52ab877146d22641976703ad0751b6
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-05-24 15:51:55 +0900

    Add Dependency Injection

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,12 +10,8 @@ import org.springframework.context.annotation.FilterType;
  // @Component는 빈에 등록 , AppConfig빼기위해 제외항목도 설정
 @Configuration
 @ComponentScan (
-		basePackages = "hello.core.member",
-		excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+//		basePackages = "hello.core.member",
+//		excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-	@Bean(name = "memoryMemberRepository")
-	 MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
 }
