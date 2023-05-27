e528227d6c13c5fab8e4fad5897c84d206e5da6f
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-05-27 18:05:00 +0900

    request scope proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -12,15 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController  {
 	private final LogDemoService logDemoService;
-	private final ObjectProvider<MyLogger> myLoggerProvider;
+//	private final ObjectProvider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
-		MyLogger myLogger = myLoggerProvider.getObject();
+//		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestURL(requestURL);
 
+		System.out.println("myLogger" + myLogger.getClass());
 		myLogger.log("controller test");
 		logDemoService.logic("testId");
 		return "OK";
